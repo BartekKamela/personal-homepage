be8904ae187193c186ecfe0a7d5a19ee800b81bb
BartekKamela <bartek@lubnet.pl>
2025-11-26 17:33:45 +0100

    Normalize.css and GlobalStyle added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyle from './globalStyles';
+import Normalize from 'react-normalize';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Normalize />
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
