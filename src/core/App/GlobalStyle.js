3bb20a6e6e4f4a1dc779fcd9bfdf2253da26965c
BartekKamela <bartek@lubnet.pl>
2025-11-26 18:46:50 +0100

    border-box added

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -1,9 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
- 
+
 const GlobalStyle = createGlobalStyle`
+  html {
+      box-sizing: border-box;
+  }
+
+  *, ::after, ::before {
+      box-sizing: inherit;
+  }
+
   body {
     background: #D1D5DA4D;
   }
 `;
- 
+
 export default GlobalStyle;
