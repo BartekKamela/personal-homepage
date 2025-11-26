79b9faa893643c008692a95d5927b0e2ee3bfc74
BartekKamela <bartek@lubnet.pl>
2025-11-26 22:59:48 +0100

    redux store added

---
+++ src/core/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import themeReducer from '../common/themeSlice';
+
+
+const store = configureStore({
+    reducer: {
+        theme: themeReducer,
+    },
+
+});
+
+
+export default store;
