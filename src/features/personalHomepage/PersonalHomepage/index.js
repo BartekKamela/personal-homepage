de29cae0b9020c19797b35871c97686c95dbf457
BartekKamela <bartek@lubnet.pl>
2025-11-28 20:32:33 +0100

    Skills component added

--- src/features/personalHomepage/PersonalHomepage/index.js
+++ src/features/personalHomepage/PersonalHomepage/index.js
@@ -1,10 +1,22 @@
 import { ThemeSwitch } from "../../../common/ThemeSwitch";
-import { MainInformation } from "./MainInformation";
 import { Container } from "./styled";
+import { MainInformation } from "./MainInformation";
+import { Skills } from "./Skills";
+import { skills } from "./skillsData";
 
 export const PersonalHomepage = () => (
     <Container>
       <ThemeSwitch />
       <MainInformation />
+      <main>
+        <Skills 
+          title={(
+            <>
+              My skillset includes
+            </>
+          )}
+          skills={skills}
+        />
+      </main>
     </Container>
 );
