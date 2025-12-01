82be51ffa1b3f9b7b64131583e622e173c409732
BartekKamela <bartek@lubnet.pl>
2025-12-01 15:13:38 +0100

    background transition added

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -11,6 +11,7 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: "Inter", sans-serif;
+    transition: background 0.3s;
     color: ${({ theme }) => theme.colors.site.text};
     background: ${({ theme }) => theme.colors.site.background};
     letter-spacing: 0.05em;
