d70be038725ceb486cf7e56c0eccedad7e18de6a
BartekKamela <bartek@lubnet.pl>
2025-12-30 15:03:29 +0100

    useEffect added with dispatch function to retrieve data from API

--- src/features/personalHomepage/PersonalHomepage/Portfolio/index.js
+++ src/features/personalHomepage/PersonalHomepage/Portfolio/index.js
@@ -1,6 +1,24 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchRepositories,
+  selectRepositories,
+  selectRepositoriesStatus,
+} from "../../personalHomepageSlice";
 import { Section, Header, StyledGithubIcon, Title, SubTitle } from './styled';
 
 export const Portfolio = () => {
+  const dispatch = useDispatch();
+  const repositories = useSelector(selectRepositories);
+  const status = useSelector(selectRepositoriesStatus);
+
+  useEffect(() => {
+    dispatch(fetchRepositories());
+  }, [dispatch]);
+
+  if (status === "success") {
+    console.log(repositories);
+  }
 
   return (
     <Section>
