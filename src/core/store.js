df8b15b5021af27d0e1e87f763f04d7ae110d96a
BartekKamela <bartek@lubnet.pl>
2025-12-30 15:02:03 +0100

    dodano personalHomePageReducer do Store oraz sagaMidellware

--- src/core/store.js
+++ src/core/store.js
@@ -1,13 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
-import themeReducer from '../common/themeSlice';
+import createSagaMiddleware from "redux-saga";
+import themeReducer from "../common/themeSlice";
+import personalHomePageReducer from "../features/personalHomepage/personalHomepageSlice";
+import rootSaga from "../core/rootSaga";
 
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         theme: themeReducer,
+        personalHomepage: personalHomePageReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 
 });
 
+sagaMiddleware.run(rootSaga);
 
 export default store;
