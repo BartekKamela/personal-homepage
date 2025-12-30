afdc7452b586c5ec99a900a6476a581532cfd703
BartekKamela <bartek@lubnet.pl>
2025-12-30 16:16:38 +0100

    added portfolio content with styles

--- src/features/personalHomepage/PersonalHomepage/Portfolio/index.js
+++ src/features/personalHomepage/PersonalHomepage/Portfolio/index.js
@@ -8,6 +8,7 @@ import {
 import { Section, Header, StyledGithubIcon, Title, SubTitle } from './styled';
 import { ErrorPage } from "./ErrorPage";
 import { Loader } from './Loader';
+import { Content, Tile, TileTitle, TileDescription, LinkWrapper, Link } from './styled';
 
 export const Portfolio = () => {
   const dispatch = useDispatch();
@@ -27,6 +28,35 @@ export const Portfolio = () => {
       </Header>
       {status === "loading" && <Loader />}
       {status === "error" && <ErrorPage />}
+      {status === "success" && (
+        <Content>
+          {repositories.map(({ id, name, description, html_url, homepage }) => (
+            <Tile key={id}>
+              <TileTitle>{name}</TileTitle>
+              <TileDescription>
+                {description ||
+                  "No description available for this project yet. I will add it soon!"}
+              </TileDescription>
+
+              {homepage && (
+                <LinkWrapper>
+                  <span>Demo:</span>
+                  <Link href={homepage} target="_blank" rel="noreferrer">
+                    {homepage ? homepage : "No demo available"}
+                  </Link>
+                </LinkWrapper>
+              )}
+
+              <LinkWrapper>
+                <span>Code:</span>
+                <Link href={html_url} target="_blank" rel="noreferrer">
+                  {html_url}
+                </Link>
+              </LinkWrapper>
+            </Tile>
+          ))}
+        </Content>
+      )}
     </Section>
   )
 }
--- src/features/personalHomepage/PersonalHomepage/Portfolio/styled.js
+++ src/features/personalHomepage/PersonalHomepage/Portfolio/styled.js
@@ -34,3 +34,66 @@ export const SubTitle = styled.h2`
   letter-spacing: 5%;
   line-height: 140%;
 `;
+
+export const Content = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 32px;
+  width: 100%;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    grid-template-columns: 1fr;
+    gap: 16px;
+  }
+`;
+
+export const Tile = styled.article`
+  background: ${({ theme }) => theme.colors.boxBackground};
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  border: 6px solid ${({ theme }) => theme.colors.iron};
+  border-radius: 4px;
+  padding: 56px;
+  transition: background 0.3s ease, border-color 0.3s ease;
+
+  &:hover {
+    border-color: ${({ theme }) => theme.colors.accent}33;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    padding: 24px;
+  }
+`;
+
+export const TileTitle = styled.h3`
+  font-size: 24px;
+  margin: 0;
+  transition: color 0.3s ease;
+`;
+
+export const TileDescription = styled.p`
+  font-size: 18px;
+  line-height: 1.4;
+  margin: 24px 0;
+  color: ${({ theme }) => theme.colors.textSecondary};
+  transition: color 0.3s ease;
+`;
+
+export const LinkWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 8px;
+  font-size: 18px;
+  margin-top: 8px;
+  color: ${({ theme }) => theme.colors.textSecondary};
+`;
+
+export const Link = styled.a`
+  color: ${({ theme }) => theme.colors.accent};
+  text-decoration: none;
+  border-bottom: 1px solid rgba(3, 102, 214, 0.2);
+  transition: 0.3s;
+
+  &:hover {
+    border-bottom: 1px solid ${({ theme }) => theme.colors.accent};
+  }
+`;
